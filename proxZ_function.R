a909fa1f8e83f05c1ee66aaea4f1e400c85f2ad7
HaeinCho <84130229+haein0224@users.noreply.github.com>
2022-03-16 03:09:01 +0900

    Update proxZ_function.R

--- proxZ_function.R
+++ proxZ_function.R
@@ -4,23 +4,22 @@ proxZ <- function(R, w, gamma, eta, K) {
   idx <- order(dt$y) # original order를 저장
   y <- sort(dt$y)
   
-  n_gamma <- dt[idx,]$gamma #rep(0, length(gamma))
-  #n_gamma <- gamma # 감마도 index따라 정렬해서 사용. -> 근데 지금 제대로 안먹음... 슈방
+  n_gamma <- dt[idx,]$gamma 
   
-  #A <- matrix(0, ncol(R), ncol(R))
+  
   A <- diag(1,ncol(R), ncol(R))
   for ( i in 1:ncol(R)) { A[i, i-1] <- -1 }
   A <- A[2:ncol(R),]
-  #A <- rbind(diag(1,ncol(R),ncol(R)), A) # 모든게 맞다고 값을 constraint로 넣은 경우 -> 근데 이러면 안되는거 아닌감?.?
-  #bvec <- c(y,rep(0,ncol(R)-1))
+  
   bvec <- rep(0,ncol(R)-1)
   kvec <- c(rep(1/K, K),rep(0, ncol(R)-K))
   dvec <- ( kvec + as.vector(eta)*y - n_gamma)
   Dmat <- diag(eta, ncol(R), ncol(R))
   
-  # 계산
+  # solve QP
   res <- solve.QP(Dmat,dvec, t(A), bvec)$solution #, meq = ncol(R))$solution
-  z[idx] <- res
-  # z를 원래 인덱스로 되돌리기
-  return(z)
+  
+  z[idx] <- res # z를 원래 인덱스로 되돌리기
+  
+  return(z) # z update 완료
 }
